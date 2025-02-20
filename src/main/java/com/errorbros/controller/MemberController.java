4217188e2133668558852260d1ea315f7056ac57
JGH <jkh6535364@gmail.com>
2025-02-20 11:40:50 +0900

    아

--- src/main/java/com/errorbros/controller/MemberController.java
+++ src/main/java/com/errorbros/controller/MemberController.java
@@ -1,5 +1,21 @@
 package com.errorbros.controller;
 
-public class MemberController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.errorbros.mapper.MemberMapper;
+
+@Controller
+public class MemberController {
+	
+	@Autowired
+	private MemberMapper memberMapper;
+	
+	@RequestMapping("/goMain")
+	public String goMain() {
+		return "Main";
+	}
 }
--- src/main/java/com/errorbros/entity/MemberDTO.java
+++ src/main/java/com/errorbros/entity/MemberDTO.java
@@ -1,5 +1,31 @@
 package com.errorbros.entity;
 
-public class MemberDTO {
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class MemberDTO {
+	
+	@NonNull
+	private String mem_id;
+	private String mem_pw;
+	private String mem_nm;
+	private String mem_phone;
+	private String mem_email;
+	private String mem_role;
+	private String mem_birthdate; 	// 데이터타입
+	private String mem_gender;		// 데이터타입
+	private String joined_at;		// 데이터타입
+	
 }
